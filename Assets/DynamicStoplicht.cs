01e6efd05b514d39bf8c6d49f12ea2dab51db225
Mikko5 <1035719@hr.nl>
2023-06-19 18:43:56 +0200

    Revert "dynamic stoplichten"
    This reverts commit a974b8ca463a762c477b44981925f41d7f85749e.

--- Assets/DynamicStoplicht.cs
+++ Assets/DynamicStoplicht.cs
@@ -7,59 +7,19 @@ public class DynamicStoplicht : MonoBehaviour
 
     public StoplichtController stoplichtController;
 
-    [SerializeField]
     public GameObject[] dynamicStoplicht;
-    public bool wait = true;
-    public int index = 0;
-    public bool active = true;
     // Start is called before the first frame update
     
     void Start()
     {
-        dynamicStoplicht[0].GetComponent<StoplichtController>().Groen();
-        for(int i = 1; i < dynamicStoplicht.Length; i++)
-        {
-            dynamicStoplicht[i].GetComponent<StoplichtController>().Rood();
-        }
+        
+        
     }
 
     // Update is called once per frame
-    
-        void Update()
-        {   
-            if(active == true)
-            {
-                Switch();
-            }
-        }
-
-        private void Switch()
-        {
-            active = false;
-            if(index > dynamicStoplicht.Length -1)
-            {
-                index = 0;
-             }
-            if(wait == true)
-            {       
-                    StartCoroutine(waiter(5));
-                    dynamicStoplicht[index].GetComponent<StoplichtController>().Rood();
-                    index++;
-                    if(index > dynamicStoplicht.Length -1)
-                    {
-                        index = 0;
-                    }
-                    dynamicStoplicht[index].GetComponent<StoplichtController>().Groen();
-                    print(index);
-
-            }
-            active = true;
-        }
-        private IEnumerator waiter(int Time)
-        {
-            wait = false;
-            yield return new WaitForSeconds(Time);
-            print("waited");
-            wait = true;
-        }      
+    void Update()
+    {
+        int index = dynamicStoplicht[1].GetComponent<StoplichtController>().indexpublic; // Accessing public variable indexpublic
+        print(index);
+    }
 }
